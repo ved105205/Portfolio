f2fb6f0856cafe5d67d24fe234654b289b74e5d4
ved105205 <vedant.nehal@mscds.christuniversity.in>
2023-12-06 16:14:18 +0530

    upload

---
+++ script.js
@@ -0,0 +1,10 @@
+setTimeout(function(){
+var typed = new Typed('.element', {
+    strings: ["Vedant Nehal"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 1000
+});
+},1000);
